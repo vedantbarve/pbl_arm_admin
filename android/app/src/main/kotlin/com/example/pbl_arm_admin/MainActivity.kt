edf844a67e4d4252cc51ae210a61bdba5d52d8c0
Vedant Barve <barve.vedant@outlook.com>
2022-06-03 22:22:07 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/pbl_arm_admin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pbl_arm_admin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const RootWidget(),
+  );
+}
+
+class RootWidget extends StatelessWidget {
+  const RootWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
