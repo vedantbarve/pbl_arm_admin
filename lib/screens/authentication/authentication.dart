b0c152fcdb5304487353d988fe33ce7d9fa7b13a
Vedant Barve <barve.vedant@outlook.com>
2022-06-09 18:01:32 +0530

    enable authentication

---
+++ lib/screens/authentication/authentication.dart
@@ -0,0 +1,101 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class AuthView extends StatefulWidget {
+  const AuthView({Key? key}) : super(key: key);
+
+  @override
+  State<AuthView> createState() => _AuthViewState();
+}
+
+class _AuthViewState extends State<AuthView> {
+  final formKey = GlobalKey<FormState>();
+  final email = TextEditingController();
+  final password = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('PBL-ARM'),
+        ),
+        body: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              const SizedBox(height: 150),
+              const Padding(
+                padding: EdgeInsets.only(
+                  top: 8.0,
+                  left: 8.0,
+                ),
+                child: Text(
+                  'SignIn',
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'This field cannot be empty';
+                    }
+                    return null;
+                  },
+                  controller: email,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    hintText: 'Email',
+                    contentPadding: const EdgeInsets.all(8.0),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'This field cannot be empty';
+                    }
+                    return null;
+                  },
+                  controller: password,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    hintText: 'Password',
+                    contentPadding: const EdgeInsets.all(8.0),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                ),
+              ),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () async {
+                    try {
+                      await FirebaseAuth.instance.signInWithEmailAndPassword(
+                        email: email.text,
+                        password: password.text,
+                      );
+                    } catch (err) {
+                      debugPrint(err.toString());
+                    }
+                  },
+                  child: const Text('SignIn'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/landing.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:pbl_arm_admin/screens/authentication/authentication.dart';
+import 'package:pbl_arm_admin/screens/home/home.dart';
+
+class LandingWidget extends StatelessWidget {
+  const LandingWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.userChanges(),
+      builder: (context, AsyncSnapshot<User?> snapshot) {
+        if (snapshot.data == null) {
+          return const AuthView();
+        }
+        if (snapshot.data != null) {
+          return const HomeView();
+        }
+        return Container();
+      },
+    );
+  }
+}
